2b7d3272698d003733ab8881bfadb1fa1595d5bb
root <root@ip-172-31-18-240.us-east-2.compute.internal>
2021-10-26 21:34:45 +0000

    new

--- distribution/setup_repo.sh
+++ distribution/setup_repo.sh
@@ -3,6 +3,7 @@
 # Perform a simple recursive find-and-replace on all variables defined in setup.conf
 export SETUP_CONF_PATH=$1 # location of the setup config
 export DISTRIBUTION_PATH=. # folder where the distribution's YAML files are to be found
+export SERVER=$2
 
 while IFS="=" read PLACEHOLDER VALUE # While loop that will perform simple parsing. On each line MY_VAR=123 will be read into PLACEHOLDER=MY_VAR, VALUE=123
 do
